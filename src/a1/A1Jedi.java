a6ddb82eceda1567d99b2b68f2bd737ec862c4ac
marce <marce@DESKTOP-KORC20C.wireless-1x.unc.edu>
2019-08-30 19:02:26 -0400

    changed tabs -> spaces

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,106 +8,106 @@ import java.util.Scanner;
 
 public class A1Jedi {
 
-	static Scanner scan = new Scanner(System.in);
+    static Scanner scan = new Scanner(System.in);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int itemAmount = scan.nextInt();
+        int itemAmount = scan.nextInt();
 
-		// holds number of customers who bought each item
-		Map<String, Integer> customersBought = new HashMap<>();
-		// itemData is in format item:amount
-		Map<String, Integer> itemData = getItemData(itemAmount,
-				customersBought);
+        // holds number of customers who bought each item
+        Map<String, Integer> customersBought = new HashMap<>();
+        // itemData is in format item:amount
+        Map<String, Integer> itemData = getItemData(itemAmount,
+                customersBought);
 
-		int customerAmount = scan.nextInt();
-		String[][] itemTally = tallyItems(itemData, customersBought,
-				customerAmount);
-		printOutput(itemTally);
-	}
+        int customerAmount = scan.nextInt();
+        String[][] itemTally = tallyItems(itemData, customersBought,
+                customerAmount);
+        printOutput(itemTally);
+    }
 
-	/*
-	 * Get item data from input and stores as a map
-	 * 
-	 */
-	public static Map<String, Integer> getItemData(int itemAmount,
-			Map<String, Integer> customersBought) {
+    /*
+     * Get item data from input and stores as a map
+     * 
+     */
+    public static Map<String, Integer> getItemData(int itemAmount,
+            Map<String, Integer> customersBought) {
 
-		// LinkedHashMap preserves insertion order
-		Map<String, Integer> itemData = new LinkedHashMap<>();
+        // LinkedHashMap preserves insertion order
+        Map<String, Integer> itemData = new LinkedHashMap<>();
 
-		// read each item into the map
-		for (int i = 0; i < itemAmount; i++) {
-			String itemName = scan.next();
-			// price is never in Jedi
-			scan.nextDouble();
-			// initial amount of items is 0
-			itemData.put(itemName, 0);
-			// customersBought references same object in main()
-			customersBought.put(itemName, 0);
-		}
-		return itemData;
-	}
+        // read each item into the map
+        for (int i = 0; i < itemAmount; i++) {
+            String itemName = scan.next();
+            // price is never in Jedi
+            scan.nextDouble();
+            // initial amount of items is 0
+            itemData.put(itemName, 0);
+            // customersBought references same object in main()
+            customersBought.put(itemName, 0);
+        }
+        return itemData;
+    }
 
-	/*
-	 * tally the number of purchases and customers per item
-	 * 
-	 */
-	public static String[][] tallyItems(Map<String, Integer> itemData,
-			Map<String, Integer> customersBought, int customerAmount) {
+    /*
+     * tally the number of purchases and customers per item
+     * 
+     */
+    public static String[][] tallyItems(Map<String, Integer> itemData,
+            Map<String, Integer> customersBought, int customerAmount) {
 
-		String[][] itemTally = new String[itemData.size()][3];
+        String[][] itemTally = new String[itemData.size()][3];
 
-		for (int i = 0; i < customerAmount; i++) {
-			// scan the first last names, never used in Jedi
-			scan.next();
-			scan.next();
-			int customerPurchaseAmount = scan.nextInt();
-			
-			// array list keeps track of duplicate items
-			ArrayList<String> items = new ArrayList<String>();
-			for (int j = 0; j < customerPurchaseAmount; j++) {
-				
-				int itemAmount = scan.nextInt();
-				String itemName = scan.next();
-				int itemTotal = itemData.get(itemName);
+        for (int i = 0; i < customerAmount; i++) {
+            // scan the first last names, never used in Jedi
+            scan.next();
+            scan.next();
+            int customerPurchaseAmount = scan.nextInt();
 
-				// update the number of items bought
-				itemData.put(itemName, itemTotal + itemAmount);
-				
-				// only add customer if item is not duplicate
-				if(!items.contains(itemName)) {
-					items.add(itemName);
-					int customerTotal = customersBought.get(itemName);
-					customersBought.put(itemName, customerTotal + 1);
-				}
-			}
-		}
+            // array list keeps track of duplicate items
+            ArrayList<String> items = new ArrayList<String>();
+            for (int j = 0; j < customerPurchaseAmount; j++) {
 
-		int i = 0;
-		for (String key : itemData.keySet()) {
-			itemTally[i][0] = key;
-			itemTally[i][1] = customersBought.get(key).toString();
-			itemTally[i][2] = itemData.get(key).toString();
-			i++;
-		}
+                int itemAmount = scan.nextInt();
+                String itemName = scan.next();
+                int itemTotal = itemData.get(itemName);
 
-		return itemTally;
-	}
+                // update the number of items bought
+                itemData.put(itemName, itemTotal + itemAmount);
 
-	/*
-	 * Print output according to specification
-	 * 
-	 */
-	public static void printOutput(String[][] itemTally) {
+                // only add customer if item is not duplicate
+                if (!items.contains(itemName)) {
+                    items.add(itemName);
+                    int customerTotal = customersBought.get(itemName);
+                    customersBought.put(itemName, customerTotal + 1);
+                }
+            }
+        }
 
-		for (int i = 0; i < itemTally.length; i++) {
-			if (itemTally[i][1].equals("0")) {
-				System.out.println("No customers bought " + itemTally[i][0]);
-			} else {
-				System.out.println(itemTally[i][1] + " customers bought "
-						+ itemTally[i][2] + " " + itemTally[i][0]);
-			}
-		}
-	}
+        int i = 0;
+        for (String key : itemData.keySet()) {
+            itemTally[i][0] = key;
+            itemTally[i][1] = customersBought.get(key).toString();
+            itemTally[i][2] = itemData.get(key).toString();
+            i++;
+        }
+
+        return itemTally;
+    }
+
+    /*
+     * Print output according to specification
+     * 
+     */
+    public static void printOutput(String[][] itemTally) {
+
+        for (int i = 0; i < itemTally.length; i++) {
+            if (itemTally[i][1].equals("0")) {
+                System.out.println("No customers bought " + itemTally[i][0]);
+            } else {
+                System.out.println(itemTally[i][1] + " customers bought "
+                        + itemTally[i][2] + " " + itemTally[i][0]);
+            }
+        }
+    }
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -4,78 +4,78 @@ import java.util.Scanner;
 
 public class A1Novice {
 
-	static Scanner scan = new Scanner(System.in);
+    static Scanner scan = new Scanner(System.in);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int customerAmount = scan.nextInt();
+        int customerAmount = scan.nextInt();
 
-		// indexing for the following arrays follow the same customer for each
-		// index
-		// [first]
-		String customerNames[][] = new String[customerAmount][2];
+        // indexing for the following arrays follow the same customer for each
+        // index
+        // [first]
+        String customerNames[][] = new String[customerAmount][2];
 
-		// [no. of purchases]
-		int customerPurchaseAmount[] = new int[customerAmount];
+        // [no. of purchases]
+        int customerPurchaseAmount[] = new int[customerAmount];
 
-		// [total of each customer]
-		double totals[] = new double[customerAmount];
+        // [total of each customer]
+        double totals[] = new double[customerAmount];
 
-		for (int i = 0; i < customerAmount; i++) {
+        for (int i = 0; i < customerAmount; i++) {
 
-			customerNames[i][0] = scan.next();
-			customerNames[i][1] = scan.next();
-			customerPurchaseAmount[i] = scan.nextInt();
+            customerNames[i][0] = scan.next();
+            customerNames[i][1] = scan.next();
+            customerPurchaseAmount[i] = scan.nextInt();
 
-			// get total for this customer
-			totals[i] = getTotal(customerPurchaseAmount[i]);
-		}
-		String formattedNames[] = formatNames(customerNames);
-		printOutput(formattedNames, totals);
-	}
+            // get total for this customer
+            totals[i] = getTotal(customerPurchaseAmount[i]);
+        }
+        String formattedNames[] = formatNames(customerNames);
+        printOutput(formattedNames, totals);
+    }
 
-	/*
-	 * Formats first and last names into F. Last
-	 * 
-	 */
-	public static String[] formatNames(String customerNames[][]) {
+    /*
+     * Formats first and last names into F. Last
+     * 
+     */
+    public static String[] formatNames(String customerNames[][]) {
 
-		String formattedNames[] = new String[customerNames.length];
+        String formattedNames[] = new String[customerNames.length];
 
-		for (int i = 0; i < customerNames.length; i++) {
+        for (int i = 0; i < customerNames.length; i++) {
 
-			formattedNames[i] = "";
-			formattedNames[i] += customerNames[i][0].charAt(0);
-			formattedNames[i] += ". ";
-			formattedNames[i] += customerNames[i][1];
+            formattedNames[i] = "";
+            formattedNames[i] += customerNames[i][0].charAt(0);
+            formattedNames[i] += ". ";
+            formattedNames[i] += customerNames[i][1];
 
-		}
-		return formattedNames;
-	}
+        }
+        return formattedNames;
+    }
 
-	public static double getTotal(int customerPurchaseAmount) {
-		double total = 0.0;
-		for (int k = 0; k < customerPurchaseAmount; k++) {
-			int amount = scan.nextInt();
-			// item name is not used in Novice
-			scan.next();
-			double price = scan.nextDouble();
-			total += amount * price;
-		}
-		return total;
-	}
+    public static double getTotal(int customerPurchaseAmount) {
+        double total = 0.0;
+        for (int k = 0; k < customerPurchaseAmount; k++) {
+            int amount = scan.nextInt();
+            // item name is not used in Novice
+            scan.next();
+            double price = scan.nextDouble();
+            total += amount * price;
+        }
+        return total;
+    }
 
-	/*
-	 * Prints the output of the program name: total
-	 * 
-	 */
-	public static void printOutput(String formattedNames[], double totals[]) {
+    /*
+     * Prints the output of the program name: total
+     * 
+     */
+    public static void printOutput(String formattedNames[], double totals[]) {
 
-		for (int i = 0; i < formattedNames.length; i++) {
-			System.out.print(formattedNames[i] + ": ");
-			// format to 2 decimal places
-			System.out.printf("%.2f", totals[i]);
-			System.out.println();
-		}
-	}
+        for (int i = 0; i < formattedNames.length; i++) {
+            System.out.print(formattedNames[i] + ": ");
+            // format to 2 decimal places
+            System.out.printf("%.2f", totals[i]);
+            System.out.println();
+        }
+    }
 }
